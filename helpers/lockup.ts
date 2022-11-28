fd6f8de751523fa27dae8388ced9adc088f20820
Ryan Walsh <rcwalsh@gmail.com>
2022-11-28 15:35:34 -0500

    added comment

--- helpers/lockup.ts
+++ helpers/lockup.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'crypto';
 
 export function getLockup(masterAccountId: string, accountId: string): string {
-  return `${sha256(accountId).slice(0, 40)}.lockup.${masterAccountId}`;
+  return `${sha256(accountId).slice(0, 40)}.lockup.${masterAccountId}`; // TODO: Link to documentation about this since it appears to be following a certain pattern.
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
