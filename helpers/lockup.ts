cb93b1ee01ad7587eee0cbcb8c0994cf27843256
Pierre Le Guen <26087574+PierreLeGuen@users.noreply.github.com>
2022-12-02 14:32:17 +0100

    Improve lockup docs.

--- helpers/lockup.ts
+++ helpers/lockup.ts
@@ -1,10 +1,14 @@
 import { createHash } from 'crypto';
 
+// LockUp account IDs are created by:
+// - hashing (sha256) the owner account ID.
+// - taking the first 40 characters of the hash.
+// - appending ".lockup.near" to the end.
+// See https://github.com/near/core-contracts/blob/f1467366930480f066ecb244881e8836db14e04e/lockup-factory/src/lib.rs#L118
 export function getLockup(masterAccountId: string, accountId: string): string {
-  return `${sha256(accountId).slice(0, 40)}.lockup.${masterAccountId}`; // TODO: Link to documentation about this since it appears to be following a certain pattern.
+  return `${sha256(accountId).slice(0, 40)}.lockup.${masterAccountId}`;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-function sha256(value: any) {
+function sha256(value: string) {
   return createHash('sha256').update(value).digest('hex');
 }
