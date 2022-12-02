f08eef8c454fbf8af18897a836d750cfb0f30011
Ryan Walsh <rcwalsh@gmail.com>
2022-12-02 11:39:55 -0500

    formatted as JSDoc

--- helpers/lockup.ts
+++ helpers/lockup.ts
@@ -1,10 +1,13 @@
 import { createHash } from 'crypto';
 
-// LockUp account IDs are created by:
-// - hashing (sha256) the owner account ID.
-// - taking the first 40 characters of the hash.
-// - appending ".lockup.near" to the end.
-// See https://github.com/near/core-contracts/blob/f1467366930480f066ecb244881e8836db14e04e/lockup-factory/src/lib.rs#L118
+/**
+ *  LockUp account IDs are created by:
+ *  - hashing (sha256) the owner account ID.
+ *  - taking the first 40 characters of the hash.
+ *  - appending ".lockup.near" to the end.
+ *
+ * See https://github.com/near/core-contracts/blob/f1467366930480f066ecb244881e8836db14e04e/lockup-factory/src/lib.rs#L118
+ */
 export function getLockup(masterAccountId: string, accountId: string): string {
   return `${sha256(accountId).slice(0, 40)}.lockup.${masterAccountId}`;
 }
